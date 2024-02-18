802885d4e38458a9175b4668bac7076421eb7ece
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-18 11:44:08 +0530

    Add files via upload

---
+++ java/Calculate.java
@@ -0,0 +1,15 @@
+class Calculate 
+{
+    public static void main(String []args)
+    {
+        double balance;
+        double rate;
+        double duration;
+        double cost;
+
+        balance =170;
+        rate=1.2;
+        duration = 
+
+    }
+}
