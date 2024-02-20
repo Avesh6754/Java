2193b05ca1cdf275a8791276056f10bf22149c1c
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-20 12:52:25 +0530

    Add files via upload

--- java/Calculate.java
+++ java/Calculate.java
@@ -9,7 +9,15 @@ class Calculate
 
         balance =170;
         rate=1.2;
-        duration = 
+        duration =37;
+        cost=duration*rate;
+        balance=balance-cost;
+
+        System.out.print("Call Duration ");
+        System.out.println(duration);
+        System.out.println("Second ");
+        System.out.print("Balabnce "+balance+"Rupees ");
+
 
     }
 }
